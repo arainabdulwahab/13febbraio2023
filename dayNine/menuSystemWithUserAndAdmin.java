4f654b433ef9a1b7b36fbd227a28ba0ca1bfed56
arainabdulwahab <113197162+arainabdulwahab@users.noreply.github.com>
2023-02-23 16:24:51 +0100

    esercizi del 23/2/2023

---
+++ dayNine/menuSystemWithUserAndAdmin.java
@@ -0,0 +1,19 @@
+package dayNine;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class menuSystemWithUserAndAdmin 
+{
+    /*
+    * Creare un sistema di isnerimento che permetta di 
+    * registrarsi tramite lo stesso metodo ma che abbia 
+    * più livelli ( almeno 3 normale, admin, capo c'è ne può 
+    * essere solo uno)
+    */    
+    // 
+    ArrayList<String> userName = new ArrayList<String>();
+    ArrayList<String> userPassword = new ArrayList<String>(); 
+    ArrayList<String> userRole = new ArrayList<String>();
+    String adminPassword = "QWODMqaos123:<@";
+    
+}
